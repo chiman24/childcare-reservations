d4ba28a48a33398a54cb5cd34fe26bf868e7afd5
CHIEDU ONWUEMENE <chieduonwuemene@CHIEDUs-MacBook-Pro-2.local>
2025-03-18 15:59:57 -0400

    Added cloudformation script.

--- backend/childcare/settings.py
+++ backend/childcare/settings.py
@@ -78,9 +78,18 @@ WSGI_APPLICATION = "childcare.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+    "default2": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    },
+
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+        "USER": "myuser",
+        "PASSWORD": "mypassword",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
