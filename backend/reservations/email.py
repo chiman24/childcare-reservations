47a87a1199d7d41a551c878edebceec97e7aecd8
CHIEDU ONWUEMENE <chieduonwuemene@CHIEDUs-MacBook-Pro-2.local>
2025-05-08 15:04:35 -0400

    Modified email to include special notes.

--- backend/reservations/email.py
+++ backend/reservations/email.py
@@ -7,11 +7,14 @@ def send_reservation_email(reservation):
     from_email = settings.DEFAULT_FROM_EMAIL
     to = [reservation.email]
 
+    special_notes = reservation.special_notes if reservation.special_notes else "No special notes provided."
+
     context = {
         "parent_name": reservation.parent_name,
         "rehearsal_date": reservation.rehearsal_date,
         "num_children": reservation.num_children,
         "child_ages": reservation.child_ages,
+        "special_notes": special_notes
     }
 
     text_content = render_to_string("emails/reservation_confirmation.txt", context)
